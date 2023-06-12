499305b08ebb5d2e32ecc50ea7ff56739050a6f8
appychip <appychipdemo@gmail.com>
2023-06-12 18:09:03 +0530

    Optimized pipeline for build and publish

--- Jenkins Job DSL/DSLjob2.groovy
+++ Jenkins Job DSL/DSLjob2.groovy
@@ -1,19 +1,22 @@
-// This job will run the node js app, build the docker image for the nodejs project and push it to the docker hub.
+job('NodeAppBuildPublish') {
+    description('AppyChip Demo for Build and Publish the Node App')
+     scm {
+        git {
+        remote {
+                url('<GITHUB_URL_FOR_THE_PROJECT>')
+         
+            }
+            wrappers {
+                preBuildCleanup()
+            } 
+            branch('*/' + 'main')
+        }
+    }
 
-job('nodeApp') {
     steps {
-        shell('''
-export BUILD_ID=dontKillMe
-IMAGE_NAME="nodeapp"
-IMAGE_TAG="2.0.0"
-DOCKERHUB_REPO="appychipDemo/jenkinsDSL"
-dir="/home/appychip/Desktop/jenkinsdsl" 
-pm2 start $dir/main.js
-docker build -t "$IMAGE_NAME:$IMAGE_TAG" $dir/
-echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
-docker tag "$IMAGE_NAME:$IMAGE_TAG" "$DOCKERHUB_REPO:$IMAGE_TAG"
-docker push "$DOCKERHUB_REPO:$IMAGE_TAG"
-
-		''')
+        dockerBuildAndPublish {
+            repositoryName('<USER_NAME>/<REPO_NAME>')
+            registryCredentials('<CREDENTIAL_ID>')
+        }
     }
 }
